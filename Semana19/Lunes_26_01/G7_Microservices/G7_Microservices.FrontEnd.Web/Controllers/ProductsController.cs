69c7f8f1fb839e2eafc6cfd5db5ff3079df862d5
Gabriel <gabrielsxf@gmail.com>
2026-01-26 22:22:45 -0500

    fix

---
+++ Semana19/Lunes_26_01/G7_Microservices/G7_Microservices.FrontEnd.Web/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using G7_Microservices.FrontEnd.Web.Models.Dto;
+using G7_Microservices.FrontEnd.Web.Models.Dto.Product;
+using G7_Microservices.FrontEnd.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace G7_Microservices.FrontEnd.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto>? listProducts = new();
+            ResponseDto? responseDto = await _productService.GetAllProductsAsync();
+            if (responseDto != null && responseDto.IsSucess)
+            {
+                listProducts = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(responseDto.Result));
+            }
+            else
+            {
+                TempData["error"] = responseDto?.Message;
+            }
+
+            return View(listProducts);
+        }
+    }
+}
